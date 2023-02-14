961a9af72c0f71b76eb29f987f28a61b01543a9a
Hector De Hoyos <hectordehoyos053@gmail.com>
2023-02-13 18:39:46 -0600

    Se creo un main

---
+++ src/components/Navegacion.jsx
@@ -0,0 +1,17 @@
+import "../App.css";
+
+function Navegacion() {
+  return (
+    <div className="navContainer">
+      <div className="titleContainer">
+        <h1 className="InnovationNav">Innovation <span className="techNav">TECH</span></h1>
+      </div>
+
+      <h2>Servicios</h2>
+      <h2>Informacion</h2>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+export default Navegacion;
